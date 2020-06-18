dc5d57e3793b387a9085deac0c1d3a2fb1acb39a
Oli <oli@Olis-MBP.home>
2020-06-18 15:47:29 -0400

    protect-ur-data-app-deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run protect_ur_data_app.py
---
+++ protect_ur_data_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+	"""a simple app"""
+	st.title("Suck ur mom")
+	st.subheader("streamlit is aite")
+
+if __name__ == '__main__':
+	main()
